afbc51432507f7a4458ee9d2678426ba5f608da7
victor <victor.freire.oliveira@gmail.com>
2023-02-05 14:07:10 -0300

    changing social links style

--- links-files/script-pt.js
+++ links-files/script-pt.js
@@ -2,14 +2,24 @@ let greetings = document.querySelector('.horario')
 let date = new Date()
 let hour = date.getHours()
 
-if (hour >= 0 && hour < 6) {
-    greetings.innerHTML = '<h2>Boa madrugada!</h2>'
+function nightSky() {
+    var btn = document.getElementsByTagName('button')
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].classList.remove("btn-day")
+        btn[i].classList.add("btn-night")
+    }
     document.querySelector('.logo').innerHTML = '<img src="links-files/logo2.svg" alt"Logo VFO Studio light">'
-    document.querySelector('.char').innerHTML = '<img src="links-files/char1.png" alt="Ilustração garota flutuante">'
     document.body.style.background = '#1e2146'
     document.querySelector('.horario').style.color = '#fff'
     document.querySelector('footer').style.color = '#fff'
     document.querySelector('.bg').style.backgroundImage = "url('links-files/bg-stars.png')"
+}
+
+
+if (hour >= 0 && hour < 6) {
+    greetings.innerHTML = '<h2>Boa madrugada!</h2>'
+    document.querySelector('.char').innerHTML = '<img src="links-files/char1.png" alt="Ilustração garota flutuante">'
+    nightSky()
 } else if (hour >= 6 && hour < 12) {
     greetings.innerHTML = '<h2>Bom dia!</h2>'
     document.body.style.background = '#a4e8e5'
@@ -22,12 +32,8 @@ if (hour >= 0 && hour < 6) {
     document.querySelector('.bg').style.backgroundImage = "url('links-files/bg2.png')"
 } else if (hour >= 19) {
     greetings.innerHTML = '<h2>Boa noite!</h2>'
-    document.querySelector('.logo').innerHTML = '<img src="links-files/logo2.svg" alt"Logo VFO Studio light">'
     document.querySelector('.char').innerHTML = '<img src="links-files/char4.png" alt="Ilustração garota flutuante">'
-    document.body.style.background = '#1e2146'
-    document.querySelector('.horario').style.color = '#fff'
-    document.querySelector('footer').style.color = '#fff'
-    document.querySelector('.bg').style.backgroundImage = "url('links-files/bg-stars.png')"
+    nightSky()
 } else {
     greetings.innerHTML = '<h2>Olá!</h2>'
     document.querySelector('.char').innerHTML = '<img src="links-files/char4.png" alt="Ilustração garota flutuante">'
