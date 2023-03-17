1422d4562259c63073ed7bbf47a42f2d7e6c72b0
victor <victor.freire.oliveira@gmail.com>
2023-03-17 17:58:40 -0300

    finished day and night mode on links page

--- src/scripts/links/links.js
+++ src/scripts/links/links.js
@@ -4,14 +4,36 @@ let hour = date.getHours();
 let year = date.getFullYear();
 
 // Greetings
+function dayTime() {
+    var dayTimeLayers = document.getElementsByClassName('light');
+
+    for (var i=0; i < dayTimeLayers.length; i++) {
+        dayTimeLayers[i].style.display = 'block'
+    }
+}
+
+function nightTime() {
+    var nightTimeLayers = document.getElementsByClassName('dark');
+    document.getElementById('bio').style.background = '#0e092e'
+
+    for (var i=0; i < nightTimeLayers.length; i++) {
+        nightTimeLayers[i].style.display = 'block'
+        greetings.style.color = '#e2d54f'
+    }
+}
+
 if (hour >= 0 && hour < 6) {
     greetings.innerHTML = '<h1>Good morning!</h1>'
+    nightTime()
 } else if (hour >= 6 && hour < 12) {
     greetings.innerHTML = '<h1>Good morning!</h1>'
+    dayTime()
 } else if (hour >= 12 && hour < 19) {
     greetings.innerHTML = '<h1>Good afternoon!</h1>'
+    dayTime()
 } else if (hour >= 19) {
     greetings.innerHTML = '<h1>Good evening!</h1>'
+    nightTime()
 }
 
 // Footer
