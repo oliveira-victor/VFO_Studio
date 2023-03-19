006125b735bfc79e4031887a854cb73bb9f4f631
victor <victor.freire.oliveira@gmail.com>
2023-03-19 20:47:52 -0300

    pt version

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -100,19 +100,24 @@ function phoneMenu() {
 // footer
 const d = new Date();
 let year = d.getFullYear();
-let language = ''
+let footerTxt = document.getElementById('footer__text');
 
 if (document.documentElement.lang === "en") {
-    language = `<p><u>English</u> | <a class="inactive-txt" href="https://vfostudio.vercel.app/pt.html">Português</a></p>`
+    footerTxt.innerHTML = `
+    &copy; ${year} VFO Studio • <nobr>by Victor Freire Oliveira</nobr>
+    <br>All rights reserved<br>
+    <a href="https://github.com/oliveira-victor" target="_blank">
+        github.com/oliveira-victor
+    </a><br><br>
+    <p><u>English</u> | <a class="inactive-txt" href="https://vfostudio.vercel.app/pt.html">Português</a></p>
+    `
 } else {
-    language = `<p><a class="inactive-txt" href="https://vfostudio.vercel.app/">English</a> | <u>Português</u></p>`
+    footerTxt.innerHTML = `
+    &copy; ${year} VFO Studio • <nobr>by Victor Freire Oliveira</nobr>
+    <br>All rights reserved<br>
+    <a href="https://github.com/oliveira-victor" target="_blank">
+        github.com/oliveira-victor
+    </a><br><br>
+    <p><a class="inactive-txt" href="https://vfostudio.vercel.app/">English</a> | <u>Português</u></p>
+    `
 }
-
-document.getElementById('footer__text').innerHTML = `
-&copy; ${year} VFO Studio • <nobr>by Victor Freire Oliveira</nobr>
-<br>All rights reserved<br>
-<a href="https://github.com/oliveira-victor" target="_blank">
-    github.com/oliveira-victor
-</a><br><br>
-${language}
-`
